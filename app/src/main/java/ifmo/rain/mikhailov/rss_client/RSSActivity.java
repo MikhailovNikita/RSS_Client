9fb3301f2c301f38483c20541fe63aa0d903278c
MikhailovNikita <mikhailovnikitau@gmail.com>
2016-12-17 01:07:38 +0300

    Project created!

---
+++ app/src/main/java/ifmo/rain/mikhailov/rss_client/RSSActivity.java
@@ -0,0 +1,13 @@
+package ifmo.rain.mikhailov.rss_client;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RSSActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rss);
+    }
+}
