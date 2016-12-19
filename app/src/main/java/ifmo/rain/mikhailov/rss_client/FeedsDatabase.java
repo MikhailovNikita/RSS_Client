3fe9a53d7f026332ee951362c0f767f2c497e0a2
MikhailovNikita <mikhailovnikitau@gmail.com>
2016-12-19 23:50:41 +0300

    Query?

--- app/src/main/java/ifmo/rain/mikhailov/rss_client/FeedsDatabase.java
+++ app/src/main/java/ifmo/rain/mikhailov/rss_client/FeedsDatabase.java
@@ -2,12 +2,15 @@ package ifmo.rain.mikhailov.rss_client;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.support.annotation.NonNull;
 import android.util.Log;
 
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Date;
 
 import static ifmo.rain.mikhailov.rss_client.DatabaseContract.RSS_CATEGORY;
 import static ifmo.rain.mikhailov.rss_client.DatabaseContract.RSS_DESCRIPTION;
@@ -45,20 +48,46 @@ public class FeedsDatabase extends SQLiteOpenHelper {
         //empty for now
     }
 
-    public void put(SQLiteDatabase sqLiteDatabase, @NonNull RSSItem item, String sourceFeed, String category){
+    public void put(SQLiteDatabase sqLiteDatabase, @NonNull RSSItem item, String sourceFeed, String category) {
         ContentValues cv = new ContentValues();
+        cv.put(RSS_CATEGORY, category);
+        cv.put(RSS_SOURCE_LINK, sourceFeed);
         cv.put(RSS_TITLE, item.getTitle());
-        cv.put(RSS_NEWS_LINK, item.getLink());
         cv.put(RSS_DESCRIPTION, item.getDescription());
         cv.put(RSS_PUB_DATE, item.getPubDate().toString());
-        cv.put(RSS_SOURCE_LINK, sourceFeed);
-        cv.put(RSS_CATEGORY, category);
+        cv.put(RSS_NEWS_LINK, item.getLink());
 
 
         sqLiteDatabase.insert(TABLE_NAME, null, cv);
     }
 
-    public void get(SQLiteDatabase sqLiteDatabase){
+    public void get(SQLiteDatabase sqLiteDatabase, String sourceLink) throws FileNotFoundException {
+        Cursor cursor = sqLiteDatabase.query(TABLE_NAME, new String[]{RSS_TITLE, RSS_DESCRIPTION, RSS_PUB_DATE, RSS_NEWS_LINK},
+                RSS_SOURCE_LINK + " = ?", new String[]{sourceLink}, null, null, RSS_PUB_DATE);
+
+        Log.d("DATABASE READING", String.valueOf(cursor.getCount()));
+
+        try {
+            if (cursor != null && cursor.moveToFirst()) {
+                ArrayList<RSSItem> rssItems = new ArrayList<>();
+
+                while (true) {
+                    RSSItem rssItem;
+
+                    int pos = 0;
+                    rssItem = new RSSItem(cursor.getString(pos++), cursor.getString(pos++),
+                            new Date(), cursor.getString(++pos));
+
+                    rssItems.add(rssItem);
+                    if(!cursor.moveToNext()) break;
+                }
+
+            }
+
+            throw new FileNotFoundException();
+        }finally {
+            if(cursor != null) cursor.close();
+        }
 
     }
 }
