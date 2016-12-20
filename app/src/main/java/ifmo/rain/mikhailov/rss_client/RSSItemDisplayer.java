8576ff6560f14ca0e8974e3cd33aad7b2e8d6f0a
AkredD <akredd@yandex.ru>
2016-12-20 21:20:58 +0300

    design v0.9 added

--- app/src/main/java/ifmo/rain/mikhailov/rss_client/RSSItemDisplayer.java
+++ app/src/main/java/ifmo/rain/mikhailov/rss_client/RSSItemDisplayer.java
@@ -1,8 +1,14 @@
 package ifmo.rain.mikhailov.rss_client;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
 import java.util.Locale;
@@ -15,24 +21,47 @@ import ifmo.rain.mikhailov.rss_client.fragments.FragmentMain;
  */
 
 public class RSSItemDisplayer extends Activity {
+    private String link;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.rss_item_displayer);
 
         RSSItem selectedRssItem = FragmentMain.selectedRssItem;
-        //Bundle extras = getIntent().getExtras();
+
         TextView titleView = (TextView) findViewById(R.id.titleTextView);
         TextView contentView = (TextView) findViewById(R.id.contentTextView);
+        FloatingActionButton fab3 = (FloatingActionButton) findViewById(R.id.fab3);
+        fab3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // --TODO ADD TO BOOKBARKS
+                FloatingActionButton v = (FloatingActionButton) view;
+                Toast.makeText(RSSItemDisplayer.this, "Запись добавлена в закладки",Toast.LENGTH_SHORT).show();
+            }
+        });
+        Button button = (Button)findViewById(R.id.go_by_link);
+        link= selectedRssItem.getLink();
         SimpleDateFormat sdf = new SimpleDateFormat("dd MMM 'at' HH:mm", Locale.ENGLISH);
-        String title = "\n" + selectedRssItem.getTitle() + " "
-                + sdf.format(selectedRssItem.getPubDate()) + " \n\n";
+        String title = "\n" + selectedRssItem.getTitle() + " \n\n";
 
-        String content = selectedRssItem.getDescription() + "\n"
-                + selectedRssItem.getLink();
+        String content = selectedRssItem.getDescription() + " \n\n"
+                + sdf.format(selectedRssItem.getPubDate()) + "\n";
+
+
 
 
         titleView.setText(title);
         contentView.setText(content);
     }
+    public void go_by_link(View view)
+    {
+
+
+        Intent browserIntent = new
+                Intent(Intent.ACTION_VIEW, Uri.parse(link));
+        startActivity(browserIntent);
+    }
+
+
 }
