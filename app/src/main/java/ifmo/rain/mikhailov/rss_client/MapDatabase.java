2451c8fc79560fbfcd89e9cb77ff02d796549e5a
MikhailovNikita <mikhailovnikitau@gmail.com>
2016-12-20 21:49:19 +0300

    Database fixes

--- app/src/main/java/ifmo/rain/mikhailov/rss_client/MapDatabase.java
+++ app/src/main/java/ifmo/rain/mikhailov/rss_client/MapDatabase.java
@@ -8,8 +8,6 @@ import android.database.sqlite.SQLiteException;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 import android.util.Pair;
-
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
